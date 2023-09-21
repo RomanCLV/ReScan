e10f356315725bbbee9b62d0dbdfb26000006bc2
RomanCLV <roman.clavier.2001@gmail.com>
2023-09-21 19:17:15 +0200

    add splash screen

--- ReScanVisualizer/Models/ItemIndexed.cs
+++ ReScanVisualizer/Models/ItemIndexed.cs
@@ -1,15 +1,13 @@
-﻿namespace ReScanVisualizer.Models
+﻿using System;
+
+#nullable enable
+
+namespace ReScanVisualizer.Models
 {
     internal class ItemIndexed<T>
     {
         public T Item { get; set; }
         public int Index { get; set; }
-
-        public ItemIndexed()
-        {
-            Item = default;
-            Index = 0;
-        }
 
         public ItemIndexed(T item, int index)
         {
